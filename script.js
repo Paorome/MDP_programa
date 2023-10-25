5751532fb8eefc785ab2855d8027d8e026bff197
PabloMur <polillomurillo@gmail.com>
2023-10-25 11:05:15 -0300

    agregue formulario de contacto / falta funcionalidad

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-const fullImgBox =document.getElementById("fullImgBox");
+const fullImgBox = document.getElementById("fullImgBox");
 fullimg = document.getElementById("fullimg");
 
-function openFullImg(reference){
+function openFullImg(reference) {
   fullImgBox.style.display = "flex";
   fullimg.src = reference;
 }
 
-function closeImg(){
+function closeImg() {
   fullImgBox.style.display = "none";
 }
+
+function enviarFormulario(e) {
+  // Prevenir que la página se recargue
+  e.preventDefault();
+
+  console.log("esto esta pasando");
+
+  // Obtener los valores de los campos
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const telefono = document.getElementById("telefono").value;
+
+  // Validar los campos (aquí puedes agregar más validaciones según tus necesidades)
+  if (nombre === "" || email === "" || telefono === "") {
+    alert("Por favor, complete todos los campos.");
+    return;
+  }
+
+  // Mostrar una alerta de éxito
+  alert("Los datos han sido enviados.");
+
+  // Limpiar los campos del formulario
+  document.getElementById("nombre").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("telefono").value = "";
+}
+
+// Agregar un evento submit al formulario
+document
+  .getElementById("solicitudTurnoForm")
+  .addEventListener("submit", enviarFormulario);
